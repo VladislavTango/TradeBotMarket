67546b73fcc3169c330a4b5252e6fad16caf22cb
VladislavTango <zalarderon00@mail.ru>
2025-02-17 19:29:07 +0300

    base architecture

---
+++ TradeBotMarket/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TradeBotMarket.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged , IDisposable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _disposed;
+        protected virtual void Dispose(bool Disposing) 
+        {
+            if (!Disposing || _disposed) return;
+            _disposed = true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? ProperyName = null) 
+        {
+            if (Equals(field, value)) return true;
+            field = value;
+
+            OnPropertyChanged(ProperyName);
+            return true;
+        }
+
+
+    }
+}
---
+++ TradeBotMarket/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using TradeBotMarket.ViewModels.Base;
+
+namespace TradeBotMarket.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "TradeBotMarket";
+        public string Title {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
