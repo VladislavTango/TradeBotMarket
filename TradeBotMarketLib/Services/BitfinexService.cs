4ebf586189305f17967be0a3f27eb79f5f5e0b85
VladislavTango <zalarderon00@mail.ru>
2025-02-24 20:02:55 +0300

    create lib

--- TradeBotMarketLib/Services/BitfinexService.cs
+++ TradeBotMarketLib/Services/BitfinexService.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http;
-using System.Net.Http.Json;
+﻿using System.Net.Http.Json;
 using System.Text.Json;
 using TradeBotMarket.Infrastructure.Converters;
 using TradeBotMarket.Models;
